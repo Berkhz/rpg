b2fb66e265709afe192e8711050bb7d32d2274a6
Kauan Bertalha <kauan.hb2004@gmail.com>
2024-06-19 21:38:32 -0300

    Up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CharacterModule } from './character/character.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), CharacterModule],
   controllers: [AppController],
   providers: [AppService],
 })
